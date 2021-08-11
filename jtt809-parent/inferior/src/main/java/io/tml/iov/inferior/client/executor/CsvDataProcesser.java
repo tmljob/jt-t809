ddc47833ac2abaf280f872efb613612ee4a9be4a
tianminliang <tianminliang@encdata.cn>
2021-08-11 10:48:17 +0800

    修改调整打包方式

--- jtt809-parent/inferior/src/main/java/io/tml/iov/inferior/client/executor/CsvDataProcesser.java
+++ jtt809-parent/inferior/src/main/java/io/tml/iov/inferior/client/executor/CsvDataProcesser.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CsvDataProcesser {
 
-    private static final String INPUT_DIR = "input";
+    private static final String INPUT_DIR = "/input";
     private static final String SUFFIX = "csv";
 
     private static final int CSV_COLUMN_LIMIT = 8;
