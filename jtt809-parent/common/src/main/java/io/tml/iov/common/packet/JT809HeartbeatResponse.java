b44969751fb0451d90f3b2ef4187c026ce2d44dd
tianminliang <tianminliang@encdata.cn>
2021-09-26 17:50:18 +0800

    JT809HeartbeatResponse在构造函数填充基本数据

--- jtt809-parent/common/src/main/java/io/tml/iov/common/packet/JT809HeartbeatResponse.java
+++ jtt809-parent/common/src/main/java/io/tml/iov/common/packet/JT809HeartbeatResponse.java
@@ -1,10 +1,23 @@
 package io.tml.iov.common.packet;
 
+import io.tml.iov.common.util.PropertiesUtil;
+import io.tml.iov.common.util.constant.Const;
+
 /**
  * 心跳应答包 数据体为空
  */
 
 public class JT809HeartbeatResponse extends JT809BasePacket {
+    
+    public JT809HeartbeatResponse() {
+        setMsgLength(JT809BasePacket.getFixedByteLength());
+        setMsgSn(Const.getMsgSN());
+        setMsgId(Const.BusinessDataType.UP_LINKTEST_RSP);
+        setMsgGNSSCenterId(PropertiesUtil.getInteger("netty.server.centerId"));
+        setVersionFlag(new byte[]{1,0,0});
+        setEncryptFlag(Const.EncryptFlag.NO);
+        setEncryptKey(0);
+    }
 
     @Override
     public byte[] getMsgBodyByteArr() {
--- jtt809-parent/superior/src/main/java/io/tml/iov/superior/handler/JT809HeartbeatHandler.java
+++ jtt809-parent/superior/src/main/java/io/tml/iov/superior/handler/JT809HeartbeatHandler.java
@@ -2,22 +2,12 @@ package io.tml.iov.superior.handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.tml.iov.common.packet.JT809BasePacket;
 import io.tml.iov.common.packet.JT809HeartbeatResponse;
-import io.tml.iov.common.util.PropertiesUtil;
-import io.tml.iov.common.util.constant.Const;
 
 
 public class JT809HeartbeatHandler extends SimpleChannelInboundHandler<JT809HeartbeatResponse> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, JT809HeartbeatResponse msg) throws Exception {
-        msg.setMsgLength(JT809BasePacket.getFixedByteLength());
-        msg.setMsgSn(Const.getMsgSN());
-        msg.setMsgId(Const.BusinessDataType.UP_LINKTEST_RSP);
-        msg.setMsgGNSSCenterId(PropertiesUtil.getInteger("netty.server.centerId"));
-        msg.setVersionFlag(new byte[]{1,0,0});
-        msg.setEncryptFlag(Const.EncryptFlag.NO);
-        msg.setEncryptKey(0);
         ctx.channel().writeAndFlush(msg);
     }
 }
